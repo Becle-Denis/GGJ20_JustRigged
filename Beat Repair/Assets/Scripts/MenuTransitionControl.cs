b8a72c690b12f6ba1689a2ab26af06c8cbb1b02c
SeanNash <c00217019@itcarlow.ie>
2020-01-31 20:37:40 +0000

    Initial Commit

---
+++ Beat Repair/Assets/Scripts/MenuTransitionControl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTransitionControl : MonoBehaviour
+{
+    private bool m_menuInView = true; //This bool controls if the menu buttons slide off screen. Used to remove the stuff to let the option screen come up.
+    private Vector3 m_inView;
+    private Vector3 m_outOfView;
+    private float m_offset = 1000f;
+    private Vector2 m_resolution;
+    private float m_transitionsRate = 20;//Move X  Pixels Per Second
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_inView = transform.position;
+        m_outOfView = m_inView;
+        m_outOfView.y = m_outOfView.y - m_offset;
+    }
+
+    public void SetTransition(bool t_transition)
+    {
+        m_menuInView = t_transition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_menuInView == false)
+        {
+            TransitionOut();
+        }
+        else
+        {
+            TransitionIn();
+        }
+    }
+
+    void TransitionOut()
+    {
+        if (transform.position.y > m_outOfView.y)
+        {
+            transform.position = transform.position - new Vector3(0, m_transitionsRate, 0);
+        }
+    }
+
+    void TransitionIn()
+    {
+        if (transform.position.y < m_inView.y)
+        {
+            transform.position = transform.position + new Vector3(0, m_transitionsRate, 0);
+        }
+    }
+}
---
+++ Beat Repair/Assets/Scripts/OptionsData.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsData
+{
+    private static float m_volume = 1;
+    private static bool m_mute = false;
+    private static bool m_volumeChanged = false;
+    private static bool m_showInstructions = true;
+
+
+    public static float Volume
+    {
+        get
+        {
+            return m_volume;
+        }
+        set
+        {
+            m_volume = value;
+        }
+    }
+
+    public static bool Mute
+    {
+        get
+        {
+            return m_mute;
+        }
+        set
+        {
+            m_mute = value;
+        }
+    }
+    public static bool VolumeChanged
+    {
+        get
+        {
+            return m_volumeChanged;
+        }
+        set
+        {
+            m_volumeChanged = value;
+        }
+    }
+
+    public static bool SeenInstructions
+    {
+        get
+        {
+            return m_showInstructions;
+        }
+        set
+        {
+            m_showInstructions = value;
+        }
+    }
+
+
+
+}
