3a2fe387af1b57bc032782a23da17669bf89708b
SeanNash <c00217019@itcarlow.ie>
2020-02-01 16:49:57 +0000

    Engine Animation, Component Initial Commit

---
+++ Beat Repair/Assets/Scripts/ComponentHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float initialHealth = 100.0f;
+
+
+    private float engineState = 100.0f;
+    private float radarState = 100.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Beat Repair/Assets/Scripts/ShipControls.cs
+++ Beat Repair/Assets/Scripts/ShipControls.cs
@@ -16,12 +16,56 @@ public class ShipControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float translation = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
-        float strafe = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
-        Vector2 movement = new Vector2(strafe, translation);       
-        transform.Translate(movement);
+        if (controllingShip)
+        {
+
+            float translation = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+            float strafe = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+            Vector2 movement = new Vector2(strafe, translation);
+            transform.Translate(movement);
+            HandleEngine();
+        }
 
     }
 
+    void HandleEngine()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        { 
+            transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.gameObject.SetActive(true);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.GetChild(0).transform.GetChild(1).transform.GetChild(0).transform.gameObject.SetActive(true);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.GetChild(0).transform.GetChild(2).transform.GetChild(0).transform.gameObject.SetActive(true);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.GetChild(0).transform.GetChild(3).transform.GetChild(0).transform.gameObject.SetActive(true);
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.gameObject.SetActive(false);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            transform.GetChild(0).transform.GetChild(1).transform.GetChild(0).transform.gameObject.SetActive(false);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            transform.GetChild(0).transform.GetChild(2).transform.GetChild(0).transform.gameObject.SetActive(false);
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            transform.GetChild(0).transform.GetChild(3).transform.GetChild(0).transform.gameObject.SetActive(false);
+        }
+
+
+    }
 
 }
