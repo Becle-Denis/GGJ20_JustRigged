279c90abf7321c735a575af24e070b6eb02d3c94
SeanNash <c00217019@itcarlow.ie>
2020-02-01 13:11:50 +0000

    Ship Partially Implemented

---
+++ Beat Repair/Assets/Scripts/ShipControls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControls : MonoBehaviour
+{
+    public float Speed = 5.0f;
+    public bool controllingShip = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+        float strafe = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+        Vector2 movement = new Vector2(strafe, translation);       
+        transform.Translate(movement);
+
+    }
+
+
+}
