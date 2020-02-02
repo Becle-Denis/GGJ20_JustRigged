108f4d4a6614e844708b06bc9e5b3dd1333f72b7
SeanNash <c00217019@itcarlow.ie>
2020-02-02 11:51:46 +0000

    Main Menu UI Implemented

---
+++ Beat Repair/Assets/Scripts/SideToSide.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideToSide : MonoBehaviour
+{
+    private Vector3 m_startPos;
+    private int m_count = 300;//Number of Frames to Apply change. One second in one direction
+    public float m_offsetPerFrame = 10;//Change this to set the offset in pixels. It will be diveded appropriately Below.
+    void Start()
+    {
+        m_offsetPerFrame = m_offsetPerFrame / 60;
+        m_startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_count--;
+        if (m_count != 0)
+        {
+            transform.position = transform.position + new Vector3(m_offsetPerFrame,0 , 0);
+        }
+        else
+        {
+            m_count = 600;//Two seconds. One to reach back to the start point, Another to move past it.
+            m_offsetPerFrame = m_offsetPerFrame * -1;
+        }
+    }
+}
---
+++ Beat Repair/Assets/Scripts/VolumeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeController : MonoBehaviour
+{
+    void Start()
+    {
+        AudioListener.volume = OptionsData.Volume;
+    }
+
+    void Update()
+    {
+       UpdateVolume(); 
+    }
+
+    void UpdateVolume()
+    { 
+        if (OptionsData.Mute)
+        {
+            AudioListener.volume = 0.0f;
+        }
+        else
+        {
+            AudioListener.volume = OptionsData.Volume;
+        }
+    }
+}
